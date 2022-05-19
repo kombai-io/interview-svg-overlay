2b0eecffe63bdd95de17a487528521b4c4c23571
Abhijit Bhole <abhijit@kombai.io>
2022-05-19 21:40:40 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  //  webpack: (config) => {
+  //   config.module.rules.push({
+  //     resourceQuery: /raw/,
+  //     type: 'asset/source',
+  //   });
+  //   config.module.rules.push({
+  //       test: /\.svg$/,
+  //       loader: 'raw-loader',
+  //   });
+
+  //   return config;
+  // }
 }
 
 module.exports = nextConfig
